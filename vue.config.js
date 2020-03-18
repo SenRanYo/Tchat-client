c949a025bd112af29687b1516ef2b3ac59d22001
unknown <1342554547@qq.com>
2020-03-18 17:24:57 +0800

    修改vue配置信息

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2020-02-11 13:58:09
- * @LastEditTime: 2020-03-01 20:20:51
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \tchat-client\vue.config.js
- */
 module.exports = {
   devServer: {
     disableHostCheck: true,
